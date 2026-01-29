a0c235dfe78ae0982c79b117f6dbb2872f05408d
Bala Penna <Bala.Penna@ril.com>
2026-01-29 22:00:42 +0530

    Added header page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
+import { Route, Routes } from 'react-router-dom'
+import Cart from './pages/Cart/Cart.jsx'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder.jsx'
+import Home from './pages/Home/Home.jsx'
 
 const App = () => {
   return (
     <div className='app'>
-      <Navbar/>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<PlaceOrder />} />
+      </Routes>
     </div>
   )
 }
